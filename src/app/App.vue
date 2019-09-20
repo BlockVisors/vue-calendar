689c7a104a1e525f04598a956ec9d5586e9ef892
Dameon Green <dameongreen@gmail.com>
2019-09-20 12:28:54 -0400

    intial

---
+++ src/app/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <CalendarHeader />
+    <CalendarWeek />
+    <CalendarEntry />
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
+import CalendarHeader from './components/CalendarHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalendarWeek,
+    CalendarEntry,
+    CalendarHeader
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+#app {
+  height: inherit;
+  background: #104480;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+</style>
+
---
+++ src/app/components/CalendarHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="calendar-head" class="container">
+    <div class="columns is-mobile">
+      <div id="header">
+        <h1>Dameon's Calendar App</h1>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from '../store.js';
+
+export default {
+  name: 'CalendarHeader',
+  data () {
+    return {
+      sharedState: store.state
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#calendar-head {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+#header {
+   font-size: 40px;
+   text-align: center;
+   color: antiquewhite; 
+}
+</style>
